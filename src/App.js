aee8b248bc7e067f0b678d19076761ff1962cc23
MinJiea <2601899252@qq.com>
2020-07-21 22:46:01 +0800

    成功

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer }from 'react';
+import React, { useState, useReducer, useRef }from 'react';
 import { Button, Input} from 'antd'
 import axios from 'axios';
 // import Mock from 'mockjs'
@@ -17,6 +17,8 @@ function reducer(state, action) {
 const initialState = {v:true};
 function App() {
 	const [state, dispatch] = useReducer(reducer, initialState);
+	const [input1,setInput1] = useState(0);
+	const [input2,setInput2] = useState(0);
 	// const data = Mock.mock(
 	// 	'/mock.json',
 	// 	'get',
@@ -25,16 +27,33 @@ function App() {
 	// 		data:true
 	// 	}
 	// )
+	const input11 = useRef();
+	const input22 = useRef();
+	const Change = () => {
+		axios.get('/mock.json').then((res)=> {	
+			res.data.id == input11.current&&res.data.password == input22.current?
+			// console.log(1):
+			dispatch({type:'login'}):
+			console.log('wu')
+		})
+	};
 	return (
 	<div>
-		<Input placeholder="账号" 
-		innerRef={(input) => this.account = input}
+		<Input placeholder="账号" onChange={e=>{
+			input11.current = e.target.value;
+			setInput1(e.target.value);
+		}}
+		// ref = {input1}
 		></Input>
-		<Input placeholder="密码" 
-		innerRef={(input) => this.password = input}
+		<Input placeholder="密码"  onChange={e=>{
+			input22.current = e.target.value;
+			setInput2(e.target.value);
+		}}
+		// ref = {input2}
+		// ref={(input) => this.password = input}
 		></Input>
-		<Button type="primary" onClick={
-			()=>{(axios.get('/mock.json').then((res)=> {res.data.user.id === this.account.value&&res.data.user.password === this.password.value?dispatch({type:'login'}):console.log('wu')}))}
+		<Button type="primary" onClick={Change
+			// ()=>{(axios.get('/mock.json').then((res)=> {res.data.user.id === this.innerRefs.value&&res.data.user.password === this.password.value?dispatch({type:'login'}):console.log('wu')}))}
 		}>登录</Button>
 		{
 			state.v ?
--- src/mock.js
+++ src/mock.js
@@ -1,8 +1,5 @@
 import Mock from 'mockjs'
 
 Mock.mock("/mock.json","get",{
-	'user|5-10':[{
-		"id|+1":1,
-		"password|18-28":0,
-	}]
+	"id":1,"password":1
 })
